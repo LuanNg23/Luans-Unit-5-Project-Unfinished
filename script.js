429f0acff9c652182668b9cc3f7a971361543ec5
LuanNg23 <127904872+LuanNg23@users.noreply.github.com>
2023-05-25 16:26:44 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+let finale= document.querySelector(".result");
+let input1;
+let input2;
+let button= document.querySelector(".submit");
+button.onclick=function(){
+    input1= document.querySelector(".answer1").value;
+input2= document.querySelector(".answer2").value;
+    console.log(input2);
+    console.log(input1);
+    
+
+};
